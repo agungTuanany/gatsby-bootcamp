6084b10d7841f01af783cac75eb907251aadc32b
agungTuanany <agung.tuanany@gmail.com>
2019-05-05 06:51:22 +0700

    Styling Gatsby Projects

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,9 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 
+/* scss */
+import '../styles/index.scss'
+
 const Layout = (props) => {
   return (
     <div>
