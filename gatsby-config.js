2be56686bf8669769fd36e91c3d23f6be11e40ae
agungTuanany <agung.tuanany@gmail.com>
2019-05-07 14:36:00 +0700

    Getting Started with Contentful| Content Management System

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,13 @@ module.exports = {
           }
         ]
       }
-
-
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
     }
   ]
 }
